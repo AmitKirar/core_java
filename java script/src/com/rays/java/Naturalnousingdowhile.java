0e481b6200deb9dac365257ed540bcc037079252
AmitKirar <amitkirar8889@gmail.com>
2025-03-21 12:06:39 +0530

    done

---
+++ java script/src/com/rays/java/Naturalnousingdowhile.java
@@ -0,0 +1,16 @@
+package com.rays.java;
+
+public class Naturalnousingdowhile {
+
+	public static void main(String[] args) {
+		int a = 1;
+		do { 
+			System.out.println( a);
+			a++;
+		}while ( a<=50);
+		
+		
+
+	}
+
+}
---
+++ java script/src/com/rays/java/Oddnousingforloop.java
@@ -0,0 +1,16 @@
+package com.rays.java;
+
+public class Oddnousingforloop {
+
+	public static void main(String[] args) {
+
+		int n=10;
+		for(int i =0; i<n; i ++) {  
+			System.out.println( 2*i+1);
+			
+		}
+			
+		
+	}
+
+}
